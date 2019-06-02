16c420e0c090fe9e862740b042c9936dabd45208
kill0rz <kill0rz@users.noreply.github.com>
2019-06-02 21:00:47 +0200

    rm: Fix Falsche Spielreihenfolge in em2020_admin.php:372

--- wbb2/acp/em2020_admin.php
+++ wbb2/acp/em2020_admin.php
@@ -363,7 +363,7 @@ if ($action == "result_add") {
 	$result = $db->query("SELECT gameid FROM bb" . $n . "_em2020_spiele WHERE team_1_id AND team_2_id AND game_goals_1 != '' AND game_goals_2 != '' AND gameid < " . intval($_REQUEST['gameid']) . " ORDER BY datetime ASC");
 
 	$entry_games_until = intval($_REQUEST['gameid']) - 1;
-	if (intval($_REQUEST['gameid']) > $gameids['vorrundenspiel'] && $db->num_rows($result) != $entry_games_until && intval($_REQUEST['gameid']) != 54 && intval($_REQUEST['gameid']) != 56) {
+	if (intval($_REQUEST['gameid']) > $gameids['vorrundenspiel'] && $db->num_rows($result) != $entry_games_until) {
 		// error
 		eval("\$tpl->output(\"" . $tpl->get('em2020_result_add_error', 1) . "\");");
 	} else {
